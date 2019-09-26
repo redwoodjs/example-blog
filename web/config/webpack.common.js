ae5982e6919438aaccd23b44db39e34bfcbc37c1
Rob Cameron <rob.cameron@fastmail.com>
2019-09-26 12:33:48 -0700

    Adds react-svg-loader config

--- web/config/webpack.common.js
+++ web/config/webpack.common.js
@@ -60,6 +60,20 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
       }
     ]
   },
