42241f1b1e2ac59c51b1f220e3e80a5a621fc575
Rob Cameron <rob.cameron@fastmail.com>
2019-10-07 10:38:04 -0700

    Require syntax update

--- api/prisma/seeds.js
+++ api/prisma/seeds.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
 
-const Photon = require('@generated/photon')
+const { Photon } = require('@generated/photon')
 const photon = new Photon()
 
-async function asyncForEach (array, callback) {
+async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array)
   }
 }
 
-async function main () {
+async function main() {
   const tagData = [
     {
       name: 'wood',
