952ac2263f7d2cfd25ea6d735a10891a99b66575
Rob Cameron <rob.cameron@fastmail.com>
2019-10-30 17:17:19 -0700

    Adds Netlify Identity widget to access admin

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,15 @@
 import ReactDOM from 'react-dom'
 import { HammerProvider } from '@hammerframework/hammer-web'
+import netlifyIdentity from 'netlify-identity-widget'
 
 import Routes from 'src/Routes'
 
 import './index.css'
+
+if (process.env.USE_AUTHENTICATION === 'true') {
+  window.netlifyIdentity = netlifyIdentity
+  netlifyIdentity.init()
+}
 
 ReactDOM.render(
   <HammerProvider>
--- web/src/layouts/AdminLayout/AdminLayout.js
+++ web/src/layouts/AdminLayout/AdminLayout.js
@@ -1,6 +1,28 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import netlifyIdentity from 'netlify-identity-widget'
 
 const AdminLayout = (props) => {
+  const [currentUser, setCurrentUser] = useState(netlifyIdentity.currentUser())
+  const isAuthEnabled = process.env.USE_AUTHENTICATION === 'true'
+
+  const signin = () => {
+    netlifyIdentity.open()
+    netlifyIdentity.on('login', (user) => {
+      setCurrentUser(user)
+      netlifyIdentity.close()
+    })
+  }
+
+  const signout = (event) => {
+    event.preventDefault()
+
+    netlifyIdentity.logout()
+    netlifyIdentity.on('logout', () => {
+      setCurrentUser(null)
+    })
+  }
+
   return (
     <div className="mx-8">
       <header className="block md:flex justify-between items-center pt-4 pb-4 px-8 border-b">
@@ -15,36 +37,58 @@ const AdminLayout = (props) => {
           </h1>
         </div>
         <nav className="text-right">
-          <Link
-            to="/signout"
-            className="text-indigo-500 hover:text-indigo-800 text-xs uppercase"
-          >
-            Sign Out
-          </Link>
+          {currentUser ? (
+            <a
+              href="#"
+              onClick={signout}
+              className="text-indigo-500 hover:text-indigo-800 text-xs uppercase"
+            >
+              Sign Out
+            </a>
+          ) : (
+            signin()
+          )}
         </nav>
       </header>
-      <main className="flex mt-2 px-8 pb-8">
-        <aside className="hidden md:block w-64 mt-4">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/admin" className="font-semibold text-indigo-700">
-                  All Posts
-                </Link>
-              </li>
-              <li className="mt-8">
-                <Link
-                  to="/admin/new"
-                  className="bg-indigo-700 hover:bg-indigo-600 text-white text-xs px-3 py-2 uppercase tracking-wide rounded"
-                >
-                  + New Post
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <section className="flex-1 mt-4">{props.children}</section>
-      </main>
+      {!isAuthEnabled || currentUser ? (
+        <main className="flex mt-2 px-8 pb-8">
+          <aside className="hidden md:block w-64 mt-4">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/admin" className="font-semibold text-indigo-700">
+                    All Posts
+                  </Link>
+                </li>
+                <li className="mt-8">
+                  <Link
+                    to="/admin/new"
+                    className="bg-indigo-700 hover:bg-indigo-600 text-white text-xs px-3 py-2 uppercase tracking-wide rounded"
+                  >
+                    + New Post
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+          <section className="flex-1 mt-4">{props.children}</section>
+        </main>
+      ) : (
+        <p className="mt-8 text-center text-gray-500">
+          You must be{' '}
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault()
+              signin()
+            }}
+            className="underline"
+          >
+            signed in
+          </a>{' '}
+          to continue
+        </p>
+      )}
     </div>
   )
 }
