1e24658320c9d253e110a68f039b12e45e35ad32
Peter Pistorius <peter.pistorius@gmail.com>
2019-10-31 17:33:15 +0100

    Upgrade hammer deps for api.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,5 +1,5 @@
-import { graphQLServerlessFunction } from '@hammerframework/hammer-api'
 import { Photon } from '@generated/photon'
+import { server } from '@hammerframework/hammer-api'
 import { GraphQLDateTime } from 'graphql-iso-date'
 import { asNexusMethod } from 'nexus'
 
@@ -7,12 +7,11 @@ import * as posts from 'src/graphql/posts'
 import * as tags from 'src/graphql/tags'
 
 export const GQLDateTime = asNexusMethod(GraphQLDateTime, 'datetime')
+const photon = new Photon()
 
-const server = graphQLServerlessFunction({
+export const handler = server({
   context: {
-    photon: new Photon(),
+    photon,
   },
-  schemaTypes: [GQLDateTime, posts, tags],
-})
-
-export const handler = server.createHandler()
+  types: [GQLDateTime, posts, tags],
+}).createHandler()
