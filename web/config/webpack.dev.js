147c4633614728c17cef008c449713034a56c7f5
Rob Cameron <rob.cameron@fastmail.com>
2019-11-01 14:48:56 -0700

    Adds polling to webpack watch

--- web/config/webpack.dev.js
+++ web/config/webpack.dev.js
@@ -1,33 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require("path");
-const merge = require("webpack-merge");
-const Dotenv = require("dotenv-webpack");
-const { getHammerConfig } = require("@hammerframework/hammer-core");
-const escapeRegExp = require("lodash.escaperegexp");
+const path = require('path')
+const merge = require('webpack-merge')
+const Dotenv = require('dotenv-webpack')
+const { getHammerConfig } = require('@hammerframework/hammer-core')
+const escapeRegExp = require('lodash.escaperegexp')
 
-const common = require("./webpack.common.js");
+const common = require('./webpack.common.js')
 
-const hammerConfig = getHammerConfig();
+const hammerConfig = getHammerConfig()
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "../dist"),
+    contentBase: path.resolve(__dirname, '../dist'),
     port: hammerConfig.web.port,
     proxy: {
       [hammerConfig.web.apiProxyPath]: {
         target: `http://localhost:${hammerConfig.api.port}`,
         pathRewrite: {
-          [`^${escapeRegExp(hammerConfig.web.apiProxyPath)}`]: ""
-        }
-      }
-    }
+          [`^${escapeRegExp(hammerConfig.web.apiProxyPath)}`]: '',
+        },
+      },
+    },
   },
   plugins: [
     new Dotenv({
-      path: `${hammerConfig.baseDir}/.env`
-    })
-  ]
-});
+      path: `${hammerConfig.baseDir}/.env`,
+    }),
+  ],
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+  },
+})
