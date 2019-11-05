2041a97c2d5200f7e590b5a58653b1f6d267dc48
Rob Cameron <rob.cameron@fastmail.com>
2019-11-04 16:12:56 -0800

    New header & logo

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,23 +1,18 @@
 import Menu from 'src/components/Menu'
+import logo from './logo.png'
 
 const BlogLayout = (props) => {
   return (
     <div className="max-w-8xl mx-auto">
       <div className="sm:mx-8">
-        <header className="md:flex flex-wrap items-center border-b-4 border-indigo-300 bg-indigo-600 text-white px-8 py-12 ">
+        <header className="md:flex flex-wrap items-center border-b-4 border-indigo-300 bg-indigo-600 text-white px-8 py-6 ">
           <div className="flex flex-1">
             <div className="flex-grow mt-1 text-center md:text-left">
-              <h1>
-                <a
-                  href="/"
-                  className="text-4xl sm:text-5xl font-semibold leading-none"
-                >
-                  Hammer Review
-                </a>
-              </h1>
-              <h2 className="text-base -mt-1 font-light text-indigo-200">
-                All the hammers
-              </h2>
+              <img
+                className="w-56 inline-block"
+                src={logo}
+                alt="Hammer Review Logo"
+              />
             </div>
           </div>
           <nav className="flex-grow sm:flex-grow-0 min-h-screenmt-4 mt-4 md:mt-0">
