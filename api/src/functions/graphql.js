ce245ce6e345a260e33c8bf4c0b3fd4a62e6f008
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-17 18:09:34 +0100

    Use hammer api schema and resolver functionality.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,20 +1,19 @@
 import { Photon } from '@generated/photon'
-import { server } from '@hammerframework/hammer-api'
-import { GraphQLDateTime } from 'graphql-iso-date'
-import { asNexusMethod } from 'nexus'
+import { server, makeMergedSchema } from '@hammerframework/api'
 import dotenv from 'dotenv'
 
 import * as posts from 'src/graphql/posts'
 import * as tags from 'src/graphql/tags'
 
+const schema = makeMergedSchema([posts, tags])
+
 dotenv.config()
 
-export const GQLDateTime = asNexusMethod(GraphQLDateTime, 'datetime')
 const photon = new Photon()
 
 export const handler = server({
+  schema,
   context: {
     photon,
   },
-  types: [GQLDateTime, posts, tags],
 }).createHandler()
