a9e5cbd43474b113f5b0a89c9f836fa81afce7ca
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-17 18:09:02 +0100

    Convert posts to schema format.

--- api/src/graphql/posts.js
+++ api/src/graphql/posts.js
@@ -1,12 +1,37 @@
-import {
-  objectType,
-  queryField,
-  stringArg,
-  arg,
-  intArg,
-  mutationField,
-} from 'nexus'
-import { UserInputError } from '@hammerframework/hammer-api'
+import { gql, UserInputError } from '@hammerframework/api'
+
+export const schema = gql`
+  type Post {
+    id: ID!
+    title: String!
+    slug: String!
+    body: String!
+    image: String
+    postedAt: DateTime!
+    tags: [Tag]
+  }
+
+  type Query {
+    post(slug: String, id: ID): Post
+    posts: [Post]
+    postsByTag(tag: String): [Post]
+    postsBySearch(term: String): [Post]
+  }
+
+  input TodoInput {
+    title: String!
+    slug: String!
+    author: String!
+    body: String!
+    image: String
+    postedAt: DateTime!
+  }
+  type Mutation {
+    postCreate(input: TodoInput!): Post
+    postUpdate(input: TodoInput!): Post
+    postDelete(id: ID!): Post
+  }
+`
 
 const validate = (args) => {
   if (args.slug && !args.slug.match(/^\S+$/)) {
@@ -18,110 +43,46 @@ const validate = (args) => {
   }
 }
 
-export const Post = objectType({
-  name: 'Post',
-  definition(t) {
-    t.int('id')
-    t.string('title')
-    t.string('slug')
-    t.string('author')
-    t.string('body')
-    t.string('image', { nullable: true })
-    t.datetime('postedAt', { nullable: true })
-    t.list.field('tags', {
-      type: 'Tag',
-      resolve(root, _args, { photon }) {
-        return photon.posts.findOne({ where: { id: root.id } }).tags()
-      },
-    })
+export const resolvers = {
+  Post: {
+    tags: (post) => {
+      return post.tags()
+    },
   },
-})
-
-export const postsAll = queryField('posts', {
-  type: Post,
-  list: true,
-  nullable: true,
-  resolve(_root, _args, { photon }) {
-    return photon.posts.findMany({
-      include: { tags: true },
-      orderBy: { postedAt: 'desc' },
-    })
+  Query: {
+    post: (_root, args, { photon }) => {
+      return photon.posts.findOne({ where: args })
+    },
+    posts: (_root, _args, { photon }) => {
+      return photon.posts.findMany({
+        include: { tags: true },
+        orderBy: { postedAt: 'desc' },
+      })
+    },
+    postsByTag: (_root, { tag }, { photon }) => {
+      return photon.tags.findOne({ where: { name: tag } }).posts()
+    },
+    postsBySearch: (_root, { term }, { photon }) => {
+      return photon.posts.findMany({
+        where: {
+          OR: [{ title: { contains: term } }, { body: { contains: term } }],
+        },
+      })
+    },
   },
-})
-
-export const post = queryField('post', {
-  type: Post,
-  args: { slug: stringArg(), id: intArg() },
-  nullable: false,
-  async resolve(_root, where, { photon }) {
-    return await photon.posts.findOne({ where })
+  Mutation: {
+    postCreate: (_root, { input }, { photon }) => {
+      validate(input)
+      return photon.posts.create({ data: input })
+    },
+    postUpdate: (_root, { input }, { photon }) => {
+      validate(input)
+      return photon.posts.update({ data: input, where: { id: input.id } })
+    },
+    postDelete: (_root, { id }, { photon }) => {
+      return photon.posts.delete({
+        where: { id },
+      })
+    },
   },
-})
-
-export const postsByTag = queryField('postsByTag', {
-  type: Post,
-  list: true,
-  args: { tag: stringArg() },
-  nullable: true,
-  async resolve(_root, { tag }, { photon }) {
-    return await photon.tags.findOne({ where: { name: tag } }).posts()
-  },
-})
-
-export const postSearch = queryField('postSearch', {
-  type: Post,
-  list: true,
-  args: { term: stringArg() },
-  nullable: true,
-  async resolve(_root, { term }, { photon }) {
-    return await photon.posts.findMany({
-      where: {
-        OR: [{ title: { contains: term } }, { body: { contains: term } }],
-      },
-    })
-  },
-})
-
-export const postCreate = mutationField('postCreate', {
-  type: Post,
-  args: {
-    title: stringArg({ required: true }),
-    slug: stringArg({ required: true }),
-    author: stringArg({ required: true }),
-    body: stringArg({ required: true }),
-    image: stringArg({ required: false }),
-    postedAt: arg({ type: 'DateTime', required: false }),
-  },
-  async resolve(_root, args, { photon }) {
-    validate(args)
-    return await photon.posts.create({ data: args })
-  },
-})
-
-export const postUpdate = mutationField('postUpdate', {
-  type: Post,
-  args: {
-    id: intArg({ required: true }),
-    title: stringArg({ required: false }),
-    slug: stringArg({ required: false }),
-    author: stringArg({ required: false }),
-    body: stringArg({ required: false }),
-    postedAt: arg({ type: 'DateTime', required: false }),
-  },
-  async resolve(_root, args, { photon }) {
-    validate(args)
-    return await photon.posts.update({ data: args, where: { id: args.id } })
-  },
-})
-
-export const postDelete = mutationField('postDelete', {
-  type: Post,
-  args: {
-    id: intArg({ required: true }),
-  },
-  async resolve(_root, { id }, { photon }) {
-    return await photon.posts.delete({
-      where: { id },
-    })
-  },
-})
+}
