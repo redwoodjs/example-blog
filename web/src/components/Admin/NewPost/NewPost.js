22f654593aebcdf88f196f6c015b0ad24ac796f7
Rob Cameron <rob.cameron@fastmail.com>
2019-11-19 14:12:05 -0800

    Remove cell structure from admin/new_post

--- web/src/components/Admin/NewPost/NewPost.js
+++ web/src/components/Admin/NewPost/NewPost.js
@@ -1,13 +1,35 @@
+import { useMutation } from '@hammerframework/hammer-web'
 import PostForm from 'src/components/Admin/PostForm'
 
-const NewPost = (props) => {
+const POSTS_CREATE_MUTATION = gql`
+  mutation PostsCreateMutation($input: PostInput!) {
+    postsCreate(input: $input) {
+      id
+    }
+  }
+`
+
+const NewPost = () => {
+  const [postsCreate, { loading, error }] = useMutation(POSTS_CREATE_MUTATION, {
+    onCompleted: () => {
+      location.href = '/admin'
+    },
+  })
+
+  const onSave = (data, type) => {
+    if (type === 'publish') {
+      data.postedAt = new Date()
+    }
+    postsCreate({ variables: { input: data } })
+  }
+
   return (
-    <>
+    <div>
       <h1 className="text-2xl font-semibold text-gray-600">New Post</h1>
       <div className="mt-8">
-        <PostForm {...props} save={true} />
+        <PostForm save={true} onSave={onSave} loading={loading} error={error} />
       </div>
-    </>
+    </div>
   )
 }
 
