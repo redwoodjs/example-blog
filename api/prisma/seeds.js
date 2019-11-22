95177309cc423682f53c18ee945b3852b6b492b6
Rob Cameron <rob.cameron@fastmail.com>
2019-11-21 16:32:01 -0800

    Seed some more posts

--- api/prisma/seeds.js
+++ api/prisma/seeds.js
@@ -25,6 +25,9 @@ async function main() {
     {
       name: 'woodworking',
     },
+    {
+      name: 'brass',
+    },
   ]
 
   const tags = []
@@ -42,7 +45,7 @@ async function main() {
       title: 'Crucible Tool Lump Hammer',
       slug: 'crucible-tool-lump-hammer',
       author: 'Rob Cameron',
-      postedAt: new Date('2019-08-20 12:00:00'),
+      postedAt: new Date('2019-08-10 12:00:00'),
       body:
         'The Crucible lump hammer has a 2.2 lb. hardened steel head and is ideal for assembly, ' +
         'mortising, setting holdfasts and dozens of other chores. Comfortable octagonal hickory ' +
@@ -68,8 +71,8 @@ async function main() {
         'ideal for woodworking instead of breaking up rocks. The octagonal handles are ' +
         'hand-finished and then waxed.\n\n' +
         'The Crucible lump hammer weighs 2.5 lbs. overall with a 2.2 lb. head of 4140 alloy ' +
-        'steel, hardened and tempered to Rc 28-32.  It is 11 ½” long overall with a head ' +
-        'that is 1 ½” x 1 ½” x 4”. Striking faces are smooth- ground at a 5 ½” ' +
+        'steel, hardened and tempered to Rc 28-32.  It is 11½” long overall with a head ' +
+        'that is 1½” x 1½” x 4”. Striking faces are smooth- ground at a 5½” ' +
         'radius, and finished by hand. Designed by Raney Nelson. Made in the United States.',
       image: 'https://cdn.filestackcontent.com/uBtP96HHSsa6Kv6qjgMP',
       tags: {
@@ -106,7 +109,7 @@ async function main() {
       title: 'Lie-Nielsen Cross Peen Hammer',
       slug: 'lie-nielsen-cross-peen-hammer',
       author: 'Rob Cameron',
-      postedAt: new Date('2019-08-10 12:00:00'),
+      postedAt: new Date('2019-08-20 12:00:00'),
       body:
         'Thomas Lie-Nielsen sidled up to me with a drink in his hand, a sportscoat on his back and a sly look across his face.\n\n' +
         'He opened his green jacket to reveal… a stick.\n\n' +
@@ -124,11 +127,58 @@ async function main() {
         connect: [{ id: tags[0].id }, { id: tags[1].id }, { id: tags[3].id }],
       },
     },
+    {
+      title: "Vertias Journeyman's Brass Mallet",
+      slug: 'veritas-journeymans-brass-mallet',
+      author: 'Rob Cameron',
+      postedAt: new Date('2019-10-01 12:00:00'),
+      body:
+        'This mallet can be used with a traditional grip or it can be nestled in the palm of your hand to exactly control the force used, a position preferred by carvers and sculptors or anyone doing work finer than mortising.\n\n' +
+        'Unique to this type of mallet, the brass head has a blind threaded connection rather than a through hole for the handle; this gives the same feel and force to the 1 5/8" diameter top as the sides. The final turning of the handle is done after the handle is mounted, ensuring a perfectly smooth transition between brass head and cherry wood handle.\n\n' +
+        'At 1¼ lb with an overall length of 6", it is as effective as it is graceful. Made in Canada.',
+      image: 'https://cdn.filestackcontent.com/GHj9JiUXQTeLqOA9HlfI',
+      tags: {
+        connect: [{ id: tags[0].id }, { id: tags[3].id }, { id: tags[4].id }],
+      },
+    },
+    {
+      title: 'Filson Rip Hammer',
+      slug: 'filson-rip-hammer',
+      author: 'Rob Cameron',
+      postedAt: new Date('2019-10-30 12:00:00'),
+      body:
+        'A classic Rip Hammer made in the USA with solid, one-piece U.S. steel construction. Its matte-black finish resists corrosion, making it ideal for outdoor use and wet conditions. Stacked leather handle with Filson logo. Heavy 24-oz. head with a versatile straight claw.\n\n' +
+        '* Rugged one-piece construction – no wood handle to fail\n' +
+        '* Made in USA with U.S. steel\n' +
+        '* Matte - black finish on head and neck protects against rust\n' +
+        '* Genuine stacked leather handle\n' +
+        '* 13½” length with heavy 24-oz.head and versatile straight claw',
+      image: 'https://cdn.filestackcontent.com/iDTWDVRSRmaH5Xs3M3g6',
+      tags: {
+        connect: [{ id: tags[0].id }, { id: tags[1].id }, { id: tags[3].id }],
+      },
+    },
+
+    {
+      title: "Thor's Hammer Woodworking Mallet",
+      slug: 'thors-hammer-woodworking-mallet',
+      author: 'Rob Cameron',
+      postedAt: new Date('2019-11-21 12:00:00'),
+      body:
+        "We designed this piece to be a loose representation of the Legendary Thor's Hammer, Mjolnir, with hints of the Marvel Comic & Movie adaptations of his Hammer.\n\n" +
+        "This is a fully functional Woodworking mallet. It is made of exotic wood in a replica of Thor's Hammer.\n\n" +
+        'These mallets are built to last many lifetimes. The head is made from Lignum Vitae and the handle from Padauk.',
+      image: 'https://cdn.filestackcontent.com/NEhtuXrFS4esyZUITFpr',
+      tags: {
+        connect: [{ id: tags[0].id }, { id: tags[1].id }, { id: tags[3].id }],
+      },
+    },
   ]
 
   const posts = []
 
   await asyncForEach(postData, async (post) => {
+    console.log(`Creating ${post.title}...`)
     posts.push(
       await photon.posts.create({
         data: post,
