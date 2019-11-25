24e68aaba4f7220c4f200784e4d3a5068642cd0c
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-25 08:54:36 +0100

    Use @hf/scripts webpack config.

--- web/config/webpack.dev.js
+++ web/config/webpack.dev.js
@@ -1,36 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const path = require('path')
-const merge = require('webpack-merge')
-const Dotenv = require('dotenv-webpack')
-const { getHammerConfig } = require('@hammerframework/hammer-core')
-const escapeRegExp = require('lodash.escaperegexp')
+const config = require('@hammerframework/scripts/config/webpackDevServer.config')
 
-const common = require('./webpack.common.js')
-
-const hammerConfig = getHammerConfig()
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '../dist'),
-    port: hammerConfig.web.port,
-    proxy: {
-      [hammerConfig.web.apiProxyPath]: {
-        target: `http://localhost:${hammerConfig.api.port}`,
-        pathRewrite: {
-          [`^${escapeRegExp(hammerConfig.web.apiProxyPath)}`]: '',
-        },
-      },
-    },
-  },
-  plugins: [
-    new Dotenv({
-      path: `${hammerConfig.baseDir}/.env`,
-    }),
-  ],
-  watchOptions: {
-    poll: 1000, // Check for changes every second
-  },
-})
+module.exports = config
