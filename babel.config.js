8740985182581a6395130ab57c045814a7b410bb
Peter Pistorius <peter.pistorius@gmail.com>
2019-12-24 16:24:51 +0200

    Lint some files.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  presets: ["@babel/preset-react", "@babel/typescript"],
+  presets: ['@babel/preset-react', '@babel/typescript'],
   plugins: [
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-export-default-from"],
-    ["@babel/plugin-proposal-object-rest-spread"],
-    ["@babel/plugin-proposal-optional-chaining"]
-  ]
-};
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-export-default-from'],
+    ['@babel/plugin-proposal-object-rest-spread'],
+    ['@babel/plugin-proposal-optional-chaining'],
+  ],
+}
--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: true,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  arrowParens: "always"
-};
+  arrowParens: 'always',
+}
