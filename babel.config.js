a9e4073825d8ce539dae6704f4d040cbcd9dd1b5
Peter Pistorius <peter.pistorius@gmail.com>
2020-02-04 10:42:03 +0100

    Use babel preset.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  presets: ['@babel/preset-react', '@babel/typescript'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-export-default-from'],
-    ['@babel/plugin-proposal-object-rest-spread'],
-    ['@babel/plugin-proposal-optional-chaining'],
-    'macros',
-  ],
+  presets: ['@redwoodjs/scripts/config/babel-preset'],
 }
