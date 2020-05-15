7c97920ae54a52a34e5ead3d9ba9c834cce303f4
Rob Cameron <rob.cameron@fastmail.com>
2020-05-15 15:46:47 -0700

    Remove old Netlify Identity hook

--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,11 @@
 import ReactDOM from 'react-dom'
 import { RedwoodProvider } from '@redwoodjs/web'
-import netlifyIdentity from 'netlify-identity-widget'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
 import FatalErrorBoundary from 'src/lib/FatalErrorBoundary'
 import Routes from 'src/Routes'
 
 import './index.css'
-
-if (process.env.USE_AUTHENTICATION === 'true') {
-  window.netlifyIdentity = netlifyIdentity
-  netlifyIdentity.init()
-}
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
