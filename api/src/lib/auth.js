4bc646e6dfe79e823e82ceb1c54c638d621c5210
Rob Cameron <rob.cameron@fastmail.com>
2020-05-18 13:45:08 -0700

    Lookup real user

--- api/src/lib/auth.js
+++ api/src/lib/auth.js
@@ -1,10 +1,5 @@
-// Define what you want `currentUser` to return throughout your app. For example,
-// to return a real user from your database, you could do something like:
-//
-//   export const currentuser = async ({ email }) => {
-//     return await db.user.findOne({ where: email })
-//   }
+import { db } from './db'
 
-export const getCurrentUser = async (jwt) => {
-  return jwt
+export const getCurrentUser = async ({ email }) => {
+  return await db.user.findOne({ where: { email } })
 }
