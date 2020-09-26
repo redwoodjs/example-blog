d5e1f1ef6d016844e1ef85ee3605830ad1a8ab52
David S Price <thedavid@thedavidprice.com>
2020-09-25 17:22:53 -0700

    upgrade v0.19.1; include fixes

--- web/config/postcss.config.js
+++ web/config/postcss.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 
-const { getPaths } = require('@redwoodjs/internal')
-
 module.exports = {
   plugins: [
     require('tailwindcss')(path.resolve(__dirname, '../tailwind.config.js')),
