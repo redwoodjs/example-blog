cecd7c3cbd87792268cd032c464e881f54e5a2ef
Rob Cameron <rob.cameron@fastmail.com>
2021-03-31 11:24:25 -0700

    Replace instances of findOne with findUnique

--- api/src/lib/auth.js
+++ api/src/lib/auth.js
@@ -2,7 +2,7 @@
 // to return a real user from your database, you could do something like:
 //
 //   export const currentuser = async ({ email }) => {
-//     return await db.user.findOne({ where: { email } })
+//     return await db.user.findUnique({ where: { email } })
 //   }
 
 import { AuthenticationError } from '@redwoodjs/api'
