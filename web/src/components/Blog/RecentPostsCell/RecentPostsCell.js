c9c685b3df2c1d901c091cee40d493c6bdb7446f
Daniel Choudhury <dannychoudhury@gmail.com>
2021-05-13 14:10:37 +0100

    Add loaders for cells

--- web/src/components/Blog/RecentPostsCell/RecentPostsCell.js
+++ web/src/components/Blog/RecentPostsCell/RecentPostsCell.js
@@ -1,4 +1,5 @@
 import { Link, routes } from '@redwoodjs/router'
+import InlineLoader from 'src/components/InlineLoader/InlineLoader'
 
 export const QUERY = gql`
   query ALL_POSTS_RECENT {
@@ -13,18 +14,18 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading recent posts...</div>
+export const Loading = InlineLoader
 
 export const Success = ({ allPosts }) => {
   return (
     <div className="">
       <h2 className="font-semibold text-indigo-800">Recent Hammers</h2>
-      <ul className="text-sm mt-2">
+      <ul className="mt-2 text-sm">
         {allPosts.posts.map((post) => (
           <li key={post.id} className="my-2">
             <Link
               to={routes.post({ slug: post.slug })}
-              className="text-indigo-600 hover:bg-indigo-100 rounded"
+              className="text-indigo-600 rounded hover:bg-indigo-100"
             >
               {post.title}
             </Link>
