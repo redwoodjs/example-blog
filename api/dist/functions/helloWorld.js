d76d5d221df2fb4934ab9783f53705b2167a481e
Daniel Choudhury <dannychoudhury@gmail.com>
2021-06-22 16:03:24 +0100

    Try proxy import version

---
+++ api/dist/functions/helloWorld.js
@@ -0,0 +1,5 @@
+const {handler} =  require('./helloWorld/helloWorldImported')
+
+module.exports = {
+	handler
+}
---
+++ api/dist/functions/helloWorld/helloWorldImported.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _Object$defineProperty = require("@babel/runtime-corejs3/core-js/object/define-property");
+
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
+
+_Object$defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.handler = void 0;
+
+var _stringify = _interopRequireDefault(require("@babel/runtime-corejs3/core-js/json/stringify"));
+
+var _logger = require("../../lib/logger");
+
+const handler = async (event, context) => {
+  _logger.logger.info('Invoked helloWorld function');
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: (0, _stringify.default)({
+      data: 'helloWorld imported function'
+    })
+  };
+};
+
+exports.handler = handler;
+//# sourceMappingURL=helloWorld.js.map
